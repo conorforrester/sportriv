bcc3575a41d4cb1ce0c8c53489ceab2c1cde8b6c
Conor Forrester <conor.forrester214@gmail.com>
2020-10-04 14:32:31 -0400

    First commit - initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Question from './components/QuestionComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Question />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuestionComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../App.css';
+import ReactDOM from 'react-dom';
+
+class Question extends Component {
+    render () {
+        return (
+            <div className="Question">
+                <h1>This will be the question component</h1>
+            </div>
+        )
+    }
+}
+
+export default Question;
