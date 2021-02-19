6b1aa333dd1db2f507b60091a6aafe821ce850c2
Conor Forrester <conor.forrester214@gmail.com>
2021-02-19 08:37:12 -0500

    Fix styling, install react countdown circle timer package

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -8,20 +8,26 @@ class Trivia extends Component {
     render () {
         return (
             <div className="Trivia">
-                <NavLink className="button-to-home" exact to="/">
-                    <i class="fas fa-angle-double-left"></i>
-                    Back to Home
-                </NavLink>
+                <div className="trivia-top-menu">
+                    <NavLink className="button-to-home" exact to="/">
+                        <i className="fas fa-angle-double-left"></i>
+                        Back to Home
+                    </NavLink>
+                    <div className="question-tracker">
+                        <h1>Question 1 of 10</h1>
+                    </div>
+                </div>
                 <div className="trivia-question-container">
                     <Question />
-                    <div class="user-buttons-container">
+                    <div className="br"></div>
+                    <div className="user-buttons-container">
                         <div className="trivia-multiple-choice-container">
                             <MultipleChoice />
                             <MultipleChoice />
                             <MultipleChoice />
                             <MultipleChoice />
                         </div>
-                        <div class="prev-next-buttons">
+                        <div className="prev-next-buttons">
                             <PrevNextButton action="Previous" />
                             <PrevNextButton action="Next" />
                         </div>
